5eb9a5600719bdd45f438cbcf30bf886c70fc381
alejandroSaura <alejandro.saura.villanueva@gmail.com>
2016-01-21 14:02:48 +0000

    bug fix

--- Assets/Shaders/BlockVertexShader.glsl
+++ Assets/Shaders/BlockVertexShader.glsl
@@ -1,5 +1,5 @@
 attribute vec3 aPositionVertex;
-uniform int edge_connect_list[3840];
+
 
 out vec3 localPosition;
 out mat4 MVP;
@@ -11,5 +11,5 @@ void main()
 	MVP = gl_ModelViewProjectionMatrix;
 	localPosition = aPositionVertex;	
 	
-	int i = edge_connect_list[0];
+	
 }
